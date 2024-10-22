6ef82deb5fe8e7b9d3dfc039d6cc0e5c7a0ad402
oscarz314 <111668068+oscarz314@users.noreply.github.com>
2024-10-22 15:32:47 +0000

    you are good

---
+++ src/main/java/com/example/project/CalcRunner.java
@@ -0,0 +1,8 @@
+package com.example.project;
+
+public class CalcRunner{
+    public static void main(String[] args) {
+        LinearCalculator test = new LinearCalculator("(-5,1)","(1,1)");
+        System.out.println(test.printInfo());
+    }
+}
